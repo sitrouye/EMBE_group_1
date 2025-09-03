ce13c5aa8490eb6a0cba7af6af976b58d7b539f7
sitrouye <best.elvire@gmail.com>
2025-09-03 16:55:01 +0200

    count encoder_1 change of state

--- Assignment 1.2/src/main.cpp
+++ Assignment 1.2/src/main.cpp
@@ -12,19 +12,31 @@ int main()
   encoder_1.init();
   encoder_2.init();
   int count = 0;
-
+  int state = 0;
 
   while (1)
   {
+    double ts = 7*2*100/15000*60*1000; //maximum sampling rate
+    _delay_ms(ts); //check state of the encoder every ts (ts not too big to not miss any change of state of the encoder)
     
-    _delay_ms(7*2*100/15000*60*1000);
-    if(encoder_1.is_hi()){
-      count += 1;
+    if (state == 1){
+      if (encoder_1.is_lo()){
+        count += 1;
+        state = 0;
+      }
     }
+    if (state == 0){
+      if (encoder_1.is_hi()){
+        count += 1;
+        state = 1;
+      }
+    }
+  }
+
     
 
 
-  }
+  
 
   return 0;
 }
