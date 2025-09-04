409f61829d471c2a91db1caf886b8ef36c661452
TomAri <112390244+Tomarinn@users.noreply.github.com>
2025-09-04 15:16:53 +0000

    Add files via upload

--- Assignment 1.2/src/main.cpp
+++ Assignment 1.2/src/main.cpp
@@ -1,47 +1,43 @@
-#include <util/delay.h>
-
 #include <Arduino.h>
-#include <digital.h>
+#include "digital.h"
+#include "encoder_poll.h"
 
+// PB5 = D13 (LED), PB1 = D9 (Encoder A), PB2 = D10 (Encoder B)
+Digital_out ledPin(5);
+Digital_in encoderPinA(1);
+Digital_in encoderPinB(2);
+Encoder_Poll encoder(encoderPinA, encoderPinB, ledPin);
 
+// sample every 200 microseconds
+const unsigned long samplePeriodMicroseconds = 200; // samples every 200 microseconds
+// tested for 400, 800, 1000 and 2000 microseconds
 
 int main() {
+  init();                        
+  Serial.begin(115200); //set serial communication
 
-  Serial.begin(9600);
-  // Pins: LED PB5, Encoder A=PB1, B=PB2
-  Digital_out led(5);     // D13
-  Digital_in  encoder_A(1); // D9 (PB1)
-  Digital_in  encoder_B(2); // D10 (PB2)
+  encoder.begin();               // prepare encoder + LED
 
-  led.init();
-  encoder_A.init();
-  encoder_B.init();
+  unsigned long nextSampleTime = micros(); //scheduled time for next sample
+  unsigned long nextPrintTime  = millis(); //scheduled time for next print
 
-  volatile int count = 0;
+  while (1) {
+    unsigned long currentTimeMicro = micros();  //read current time since program start
+    //unsigned long is a 32-bit number that only stores positive values.
 
-  // const uint16_t TS_US = 182;
-
-  bool last = encoder_A.is_hi();  // initial state of A
-
-while (1) {
-    _delay_ms(100);        // too slow
-    bool state = encoder_A.is_hi();  // read channel A
-
-    if (state != last) {   
-        if (encoder_B.is_lo()) {
-            count -= 1;    // one direction
-            led.toggle();  // blink LED
-        }
-        if (encoder_B.is_hi()) {
-            count += 1;    // other direction
-            led.toggle();  // blink LED
-        }
-        last = state;      // update previous state
+    // run update every samplePeriodMicroseconds
+    // position is updated every samplePeriodMicroseconds
+    if ((currentTimeMicro - nextSampleTime) >= 0) {
+      encoder.update(); //runs edge detection and position update
+      nextSampleTime += samplePeriodMicroseconds; //schedule next sample to be exactly 200millisec later not from now
     }
-    Serial.println(count);
-}
-
+    //verify position
+    //print position every 100 ms
+    if (millis() - nextPrintTime >= 100) {
+      nextPrintTime += 100;
+      Serial.print("Position = ");
+      Serial.println(encoder.position());
+    }
+  }
   return 0;
 }
-
-
