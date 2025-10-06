72b4513ca7b84d9301bf2ebb2d5ca2f0c1fdf26a
sitrouye <best.elvire@gmail.com>
2025-10-06 11:36:07 +0200

    add preoperational state

--- Project3/src/state.cpp
+++ Project3/src/state.cpp
@@ -21,11 +21,11 @@ void Operational::handleCommand(MotorController* controller, char cmd) {
         Serial.println("Resetting...");
         controller->setState(new Initialization(led));
     } 
-    else if (cmd = 'p'){
+    else if (cmd == 'p'){
         Serial.println("set pre-operational...");
         controller->setState(new PreOperational(led));
     }
-    else if (cmd = 's'){
+    else if (cmd == 's'){
         Serial.println("Stop");
         controller->setState(new Stopped(led));
     }
@@ -41,11 +41,11 @@ void Stopped::handleCommand(MotorController* controller, char cmd) {
         Serial.println("Resetting...");
         controller->setState(new Initialization(led));
     } 
-    else if (cmd = 'p'){
+    else if (cmd == 'p'){
         Serial.println("set pre-operational...");
         controller->setState(new PreOperational(led));
     }
-    else if (cmd = 'o'){
+    else if (cmd == 'o'){
         Serial.println("set operational...");
         controller->setState(new Operational(led));
     }
@@ -67,3 +67,56 @@ void Stopped::during(MotorController* controller){
     led->toggle();
     delay(250);
 }
+
+void Stopped::handleCommand(MotorController* controller, char cmd) {
+    if (cmd == 'r') {
+        Serial.println("Resetting...");
+        controller->setState(new Initialization(led));
+    } 
+    else if (cmd == 'p'){
+        Serial.println("set pre-operational...");
+        controller->setState(new PreOperational(led));
+    }
+    else if (cmd == 'o'){
+        Serial.println("set operational...");
+        controller->setState(new Operational(led));
+    }
+    
+    else {
+        Serial.println("Unknown command.");
+    }
+}
+
+
+
+void PreOperational::enter(MotorController* controller) {
+    led->on();
+    controller->stop();
+    Serial.println("System operational. Type 'r' to reset.");
+}
+
+void PreOperational::during(MotorController* controller){
+    led->toggle();
+    delay(250);
+}
+
+
+void PreOperational::handleCommand(MotorController* controller, char cmd) {
+    if (cmd == 'r') {
+        Serial.println("Resetting...");
+        controller->setState(new Initialization(led));
+    } 
+    else if (cmd == 'p'){
+        Serial.println("set pre-operational...");
+        controller->setState(new PreOperational(led));
+    }
+    else if (cmd == 'o'){
+        Serial.println("set operational...");
+        controller->setState(new Operational(led));
+    }
+    
+    else {
+        Serial.println("Unknown command.");
+    }
+}
+
